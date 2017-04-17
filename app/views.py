7f06d8535f946f2ef04b0fca8d0d01073d324690
25hours <shawnniu@yeah.net>
2017-04-18 01:12:16 +0800

    modify base.html

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template,url_for,redirect
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/table')
+def table():
+    return render_template('table.html')
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('login'))
